425c763f2ba40dc9365d4446d0cc8bf337b37e67
polibiopipe <pofelipso@hotmail.com>
2026-08-16 11:52:03 -0400

    feat: habilita Google Auth en Mi Núcleo staging

--- mi-nucleo/mi-nucleo-config.js
+++ mi-nucleo/mi-nucleo-config.js
@@ -1,4 +1,4 @@
 window.MI_NUCLEO_CONFIG = Object.freeze({
   // Activar solo después de configurar y validar Google en Supabase y Google Cloud.
-  enableGoogleAuth: false
+  enableGoogleAuth: true
 });
